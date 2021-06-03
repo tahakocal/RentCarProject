1acc12114782df7841135e6e43dc88c280e545fb
Taha Koçal <tahakocalgs@gmail.com>
2021-06-03 14:33:25 +0300

    03.06.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,7 @@
-﻿using Business.Concrete;
+﻿using System;
+using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
-using System;
 
 namespace ConsoleUI
 {
@@ -10,16 +10,14 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            carManager.Add(new Car { Id = 1, BrandId = 1, CarName = "R34", Description = "Nissan R34", ColorId = 5, ModelYear = 2001, DailyPrice = 500 });
+            carManager.Add(new Car { Id = 1, BrandId = 1, CarName = "Nissan", Description = "Nissan R34", ColorId = 5, ModelYear = 2001, DailyPrice = 500 });
+            carManager.Add(new Car { Id = 2, BrandId = 1, CarName = "BMW", Description = "BMW M4", ColorId = 1, ModelYear = 2020, DailyPrice = 0 });
 
 
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine(carManager);
-            }
-
-
-
+            //foreach (var VARIABLE in carManager.GetAll())
+            //{
+            //    Console.WriteLine(carManager);
+            //}
 
         }
     }
