122a93024c1ab449e1355ad35b0589c04fb7ab98
Taha Koçal <tahakocalgs@gmail.com>
2021-07-04 19:54:54 +0300

    04.07.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,10 +14,10 @@ namespace ConsoleUI
             carManager.Add(new Car { Id = 2, BrandId = 1, CarName = "BMW", Description = "BMW M4", ColorId = 1, ModelYear = 2020, DailyPrice = 0 });
 
 
-            //foreach (var VARIABLE in carManager.GetAll())
-            //{
-            //    Console.WriteLine(carManager);
-            //}
+            foreach (var VARIABLE in carManager.GetAll())
+            {
+                Console.WriteLine(carManager);
+            }
 
         }
     }
