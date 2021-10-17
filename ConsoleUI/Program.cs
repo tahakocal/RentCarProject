b6fe06743fc6ec8ecb70da7d128ba5e676fd11ff
Taha Koçal <tahakocalgs@gmail.com>
2021-10-17 12:57:30 +0300

    17.10.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -38,7 +38,9 @@ namespace ConsoleUI
                 DailyPrice = 500
             });
             carManager.Add(new Car
-                {Id = 2, BrandId = 1, CarName = "BMW", Description = "BMW M4", ColorId = 1, ModelYear = 2020, DailyPrice = 0});
+            {
+                Id = 2, BrandId = 1, CarName = "BMW", Description = "BMW M4", ColorId = 1, ModelYear = 2020, DailyPrice = 0
+            });
             carManager.Add(new Car
             {
                 Id = 3, BrandId = 3, CarName = "Scania", Description = "R420", ColorId = 3, ModelYear = 2019, DailyPrice = 950
